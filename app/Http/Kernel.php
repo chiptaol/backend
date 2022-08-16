9c734114e8af406aed1e462cc3412d10a5f5fff7
BakhadyrovF <BakhadyrovF@gmail.com>
2022-08-16 22:35:06 +0500

    ref: middleware renamed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \App\Http\Middleware\EncryptCookies::class,
-            \App\Http\Middleware\AppendHeadersToResponse::class,
+            \App\Http\Middleware\AppendHeadersToRequest::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
